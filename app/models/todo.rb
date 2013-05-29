b73ef7936d51ad1e064afda3514a5edb1c8e806b
Ruslan Khaertdinov <ruslan.khaertdinov@flatstack.com>
2013-05-29 23:00:30 +0400

    first steps

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,19 @@
 class Todo < ActiveRecord::Base
-  attr_accessible :boolean, :done, :integer, :order, :string, :title
+  attr_accessible :done, :order, :title, :due_date
+
+  before_save :set_order_value
+
+  scope :ordered, -> { order('created_at ASC') }
+
+  protected
+
+  def set_order_value
+    last_order = Todo.last.order if Todo.last
+    order_value = if last_order
+                    last_order + 1
+                  else
+                    0
+                  end
+    self.order = order_value
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ TodoServer::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'todos#index'
 
   # See how all your routes lay out with "rake routes"
 
